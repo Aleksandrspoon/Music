ef5afca53f06adfcd82ba3dd59c1e957bb5bf0a9
Aleksandr Kolotovkin <aleksandrspoon@gmail.com>
2023-04-14 11:09:30 +0300

    way

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import '../css/App.css';
+import './css/App.css';
 import Main from './components/Main';
 import NavMenu from './components/NavMenu';
 import StartScreen from './components/StartScreen';
